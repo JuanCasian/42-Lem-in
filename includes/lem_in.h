fa91e75fe119b46fddb09b9c5d9d60b029da4307
Juan Casian <juanpedrocasian@gmail.com>
2019-03-22 16:15:57 -0700

    adding hash_table functions

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -31,8 +31,9 @@ typedef struct		s_linked
 
 typedef struct		s_hash_item
 {
-	char			*key;
-	void			*val;
+	char				*key;
+	void				*val;
+	struct s_hash_item	*next;
 }					t_hash_item;
 
 typedef struct		s_hash_table
@@ -46,10 +47,12 @@ typedef struct		s_hash_table
 */
 
 t_hash_table	*hash_new_table(int size);
-t_hash_item		*hash_new_item();
-int				hash_insert();
-int				hash_delete();
+t_hash_item		*hash_new_item(char *key, void *val);
+int				hash_insert(t_hash_table *table, char *key, void *val);
 void			hash_display();
+t_hash_item		hash_search(t_hash_table *table, char *key);
+
+int				hash_function(char *key, int size);
 
 /*
 ** Linked list functions
--- src/hash_table.c
+++ src/hash_table.c
@@ -12,6 +12,19 @@
 
 #include "lem_in.h"
 
+int				hash_function(char *key, int size)
+{
+	int	hash;
+	int	c;
+
+	hash = 5385;
+	while (c = (int)*key++)
+	{
+		hash = ((hash << 5) + hash) + c;
+	}
+	return (hash / size);
+}
+
 t_hash_table	*hash_new_table(int size)
 {
 	t_hash_table	*hash;
@@ -32,11 +45,54 @@ t_hash_table	*hash_new_table(int size)
 	return (hash);
 }
 
-t_hash_item		*hash_new_item(char key, void *val)
+t_hash_item		*hash_new_item(char *key, void *val)
 {
 	t_hash_item	*item;
 
 	if (!(item = (t_hash_item*)malloc(sizeof(t_hash_item))))
 		return (NULL);
+	item->key = key;
+	item->val = val;
+	item->next = NULL;
+	return (item);
+}
+
+int				hash_insert(t_hash_table *table, char *key, void *val)
+{
+	t_hash_item	*item;
+	int			index;
+	t_hash_item *tmp;
+
+	if (!(item = hash_new_item(key, val)))
+		return (0);
+	index = hash_function(key, table->size);
+	tmp = table->items[index];
+	while (tmp)
+	{
+		if (!ft_strcmp(tmp->key, key))
+		{
+			free(item);
+			return (0);
+		}
+		tmp = tmp->next;
+	}
+	item->next = table->items[index];
+	table->items[index] = item->next;
+	return (1);	
+}
+
+t_hash_item		hash_search(t_hash_table *table, char *key)
+{
+	int			index;
+	t_hash_item	*tmp;
 	
+	index = hash_function(key, table->size);
+	tmp = table->items[index];
+	while (tmp)
+	{
+		if (!ft_strcmp(tmp->key, key))
+			return (tmp);
+		tmp = tmp->next;
+	}
+	return (NULL);
 }
